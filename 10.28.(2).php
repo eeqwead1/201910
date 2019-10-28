0b26a2d500724dfc288659181866f7b329d3c234
eeqwead1 <56808905+eeqwead1@users.noreply.github.com>
2019-10-28 11:46:21 +0800

    Create 10.28.(2).php

---
+++ 10.28.(2).php
@@ -0,0 +1,86 @@
+.header{
+    padding: 20px 0px;
+}
+.header .logo{
+    font-style: 10px;
+}
+.header .search-bar{
+    border: 2px solid #dd182b;
+    background-color: #dd182b;
+}
+.header .search-bar input,
+.header .search-bar button{
+    border-width: 0px;
+    padding: 10px;
+    display: block;
+    float: left;
+    outline: 0px;
+}
+.header .search-bar input{
+    width: 80%;
+}
+.header .search-bar button{
+    width: 20%;
+    background:#dd182b;
+    color: #fff;
+}
+.header .search-bar input:focus{
+    box-shadow: inset 0 0 2px right(0,0,0, 0.5);
+}
+.header .search-bar button:hover{
+    background:#90111d;
+}
+.header .cart{
+    text-align: center;
+}
+.header .cart a{
+    float: right;
+    width: 80%;
+    border:2px solid #eee;
+    padding: 10px;
+    background:#fff;
+    color: #dd182b;
+}
+.col-1,
+.col-2,
+.col-3
+{
+    float: left;
+    display: block;
+    position: relative;
+    min-height: 1px;
+}
+.col-1{
+    width: 20%;
+}
+.col-2{
+    width: 50%;
+}
+.col-3{
+    width: 30%;
+}
+a{
+    text-decoration: none;
+}
+.container{
+    max-width: 1080px;
+    margin: 0 auto;
+    display: block;
+}
+.fl{
+    float: left;
+}
+.fr                                                                                                                                         {
+    float: right;
+}
+.top-nav{
+    background:#eee;
+}
+.top-nav .item{
+    display: inline-block;
+    padding: 6px 10px;
+    color: #666;
+}
+.top-nav .ifem:hover{
+    color: #333;
+}
